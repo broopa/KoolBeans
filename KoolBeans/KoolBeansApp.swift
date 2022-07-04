8b94ea680e41f5cfc6366e05273a98b304a42161
Ian-CM <30392014+broopa@users.noreply.github.com>
2022-07-04 15:17:24 -0400

    Initial Push
    Fresh Build Push

---
+++ KoolBeans/KoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoolBeansApp.swift
+//  KoolBeans
+//
+//  Created by Ian Albahae on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct KoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
